e29db8debd9774ff1c838f94e471d550c553aaee
Shashank Kumar <112277097+OpenSourcePundit@users.noreply.github.com>
2023-05-09 01:16:40 +0530

    Ques2_adding items completed. useState used. . adding to main list completed.

---
+++ src/2-GroceryList.jsx
@@ -0,0 +1,44 @@
+// Create a list of Grocery list items with checkboxes and as the user checks out the item those checked-out items should be listed in another section named ‘Completed list’. Add the list of items via an input field.
+import { useState } from "react";
+import { React } from "react";
+
+export const GroceryList = () => {
+  let itemName;
+  let [list, setList] = useState([]);
+  let [completedList, setCompletedList] = useState([]);
+
+  const AddItem = () => {
+    if (itemName !== undefined) setList([...list, itemName]);
+    console.log(list);
+  };
+  return (
+    <div className="GroceryList Main">
+      <h2 className="GroceryList Heading">Grocery List</h2>
+      Add Item:
+      <input
+        type="text"
+        label="Add Item:"
+        className="Input"
+        id="ItemList"
+        onChange={() => (itemName = event.target.value)}
+      />
+      <button className="AddItemBtn" onClick={AddItem}>
+        Add
+      </button>
+      <div className="GroceryList SolutionScreen">
+        <ul className="List">
+          {list.map(item => {
+            return (
+              <ol className="ListItem">
+                <input type="checkbox" value={item} />
+                {item}
+              </ol>
+            );
+          })}
+        </ul>
+
+        <h3 className="CompletedListHeading">Completed List</h3>
+      </div>
+    </div>
+  );
+};
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 
 import { Tab } from "./1-Tabs";
+import { GroceryList } from "./2-GroceryList";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Assignment Three</h1>
       <Tab />
+      <GroceryList />
     </div>
   );
 }
